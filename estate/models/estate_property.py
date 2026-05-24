ba737658f0dc68a815d58c131f0cc8714626607d
gabcytn-awb <gabriel.cayetano@achievewithoutborders.com>
2026-05-24 17:15:09 +0800

    [FEAT] property: add property tags functionality

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -46,3 +46,4 @@ class EstateProperty(models.Model):
         "res.users", string="Seller", default=lambda self: self.env.user
     )
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
