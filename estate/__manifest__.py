59cab2725f43349df043d6f1fb715b56ff1af2e7
gabcytn-awb <gabriel.cayetano@achievewithoutborders.com>
2026-05-24 12:03:35 +0800

    [FEAT] estate: add basic estate management module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Estate Management",
+    "depends": ["base"],
+    "installable": True,
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+}
