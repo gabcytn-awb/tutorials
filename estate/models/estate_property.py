c6a5d396ef7772adafd3464fc3bd99d22ec3cc32
gabcytn-awb <gabriel.cayetano@achievewithoutborders.com>
2026-05-24 12:34:50 +0800

    [FEAT] estate: add new fields and update property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,12 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=fields.Date.add(fields.Date.today(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -23,4 +25,17 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
